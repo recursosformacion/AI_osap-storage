6d34bd21f5b8d08f2296d001b777b966be319862
Miguel <migarcia@recursosformacion.com>
2026-08-19 12:20:42 +0200

    Migración 026: quitar punto y coma del comentario (el runner parte el SQL por ';' y rompía el arranque)

--- infrastructure/db/migrations/026_composer_identity_resolution_widen.sql
+++ infrastructure/db/migrations/026_composer_identity_resolution_widen.sql
@@ -1,5 +1,5 @@
 -- 026_composer_identity_resolution_widen.sql
--- El nombre de compositor en works.composer es VARCHAR(1024); attribution (255) desbordaba
+-- El nombre de compositor en works.composer es VARCHAR(1024) y attribution (255) desbordaba
 -- y abortaba la pasada con DataError 1406. Se amplia attribution y decision_reason a 1024.
 
 ALTER TABLE composer_identity_resolution
